d4387302f22cb7ff6cbecf659abfa5a27965c2d8
Kalmalyzer <mikael@kalms.org>
2020-05-03 15:54:35 +0200

    Added logging to Google Cloud Storage backend (#165)
    * Added logging to Google Cloud Storage backend
    
    All successful accesses result in log output when run in --verbose mode.
    All access errors result in log errors in any mode.
    
    * gcs RemoveChunk() operation should either print error or regular message, not both
    
    * Newline changes

--- gcsindex.go
+++ gcsindex.go
@@ -6,7 +6,9 @@ import (
 	"net/url"
 	"path"
 
+	"cloud.google.com/go/storage"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // GCIndexStore is a read-write index store with Google Storage backing
@@ -28,10 +30,25 @@ func NewGCIndexStore(location *url.URL, opt StoreOptions) (s GCIndexStore, e err
 // file does not exist.
 func (s GCIndexStore) GetIndexReader(name string) (r io.ReadCloser, err error) {
 	ctx := context.TODO()
+
+	var (
+		log = Log.WithFields(logrus.Fields{
+			"bucket": s.bucket,
+			"name":   s.prefix + name,
+		})
+	)
+
 	obj, err := s.client.Object(s.prefix + name).NewReader(ctx)
-	if err != nil {
+
+	if err == storage.ErrObjectNotExist {
+		log.Warning("Unable to create reader for object in GCS bucket; the object may not exist, or the bucket may not exist, or you may not have permission to access it")
+		return nil, errors.Wrap(err, s.String())
+	} else if err != nil {
+		log.WithError(err).Error("Error when creating index reader from GCS bucket")
 		return nil, errors.Wrap(err, s.String())
 	}
+
+	log.Debug("Created index reader from GCS bucket")
 	return obj, nil
 }
 
@@ -48,13 +65,32 @@ func (s GCIndexStore) GetIndex(name string) (i Index, e error) {
 // StoreIndex writes the index file to the Google Storage store
 func (s GCIndexStore) StoreIndex(name string, idx Index) error {
 	ctx := context.TODO()
+
+	var (
+		log = Log.WithFields(logrus.Fields{
+			"bucket": s.bucket,
+			"name":   s.prefix + name,
+		})
+	)
+
 	w := s.client.Object(s.prefix + name).NewWriter(ctx)
 	w.ContentType = "application/octet-stream"
+
 	_, err := idx.WriteTo(w)
+
 	if err != nil {
+		log.WithError(err).Error("Error when copying data from local filesystem to object in GCS bucket")
 		w.Close()
 		return errors.Wrap(err, path.Base(s.Location))
 	}
+
 	err = w.Close()
-	return errors.Wrap(err, path.Base(s.Location))
+
+	if err != nil {
+		log.WithError(err).Error("Error when finalizing copying of data from local filesystem to object in GCS bucket")
+		return errors.Wrap(err, path.Base(s.Location))
+	}
+
+	log.Debug("Index written to GCS bucket")
+	return nil
 }
