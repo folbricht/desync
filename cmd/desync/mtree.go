b4d85a846be5d47e0643760d9f5e8577f63fbf7e
folbrich <frank.olbricht@gmail.com>
2020-09-26 18:10:42 -0600

    Support printing mtree of local dirs

--- cmd/desync/mtree.go
+++ cmd/desync/mtree.go
@@ -21,15 +21,17 @@ func newMtreeCommand(ctx context.Context) *cobra.Command {
 	var opt mtreeOptions
 
 	cmd := &cobra.Command{
-		Use:   "mtree <catar|index>",
-		Short: "Print the content of a catar or caidx in mtree format",
-		Long: `Reads an archive (catar) or index (caidx) and prints the content in mtree
-format.
+		Use:   "mtree <catar|index|dir>",
+		Short: "Print the content of a catar, caidx or local directory in mtree format",
+		Long: `Reads an archive (catar), index (caidx) or local directory and prints
+the content in mtree format.
 
-The input is either a catar archive, or a caidx index file (with -i and -s).
+The input is either a catar archive, a caidx index file (with -i and -s), or
+a local directory.
 `,
 		Example: `  desync mtree docs.catar
-  desync mtree -s http://192.168.1.1/ -c /path/to/local -i docs.caidx`,
+  desync mtree -s http://192.168.1.1/ -c /path/to/local -i docs.caidx
+  desync mtree /path/to/dir`,
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return runMtree(ctx, opt, args)
@@ -53,9 +55,38 @@ func runMtree(ctx context.Context, opt mtreeOptions, args []string) error {
 	}
 
 	input := args[0]
-	fs, err := desync.NewMtreeFS(os.Stdout)
+	mtreeFS, err := desync.NewMtreeFS(os.Stdout)
 	if err != nil {
 		return err
+	}
+
+	stat, err := os.Stat(input)
+	if err != nil {
+		return err
+	}
+
+	if opt.readIndex && stat.IsDir() {
+		return errors.New("-i can't be used with input directory")
+	}
+
+	// Input is a directory, not an archive. So Tar it into an Untar stream
+	// which then writes into an mtree writer.
+	if stat.IsDir() {
+		r, w := io.Pipe()
+		inFS := desync.NewLocalFS(input, desync.LocalFSOptions{})
+
+		// Run the tar bit in a goroutine, writing to the pipe
+		var tarErr error
+		go func() {
+			tarErr = desync.Tar(ctx, w, inFS)
+			w.Close()
+		}()
+		untarErr := desync.UnTar(ctx, r, mtreeFS)
+
+		if tarErr != nil {
+			return tarErr
+		}
+		return untarErr
 	}
 
 	// If we got a catar file unpack that and exit
@@ -66,7 +97,7 @@ func runMtree(ctx context.Context, opt mtreeOptions, args []string) error {
 		}
 		defer f.Close()
 		var r io.Reader = f
-		return desync.UnTar(ctx, r, fs)
+		return desync.UnTar(ctx, r, mtreeFS)
 	}
 
 	s, err := MultiStoreWithCache(opt.cmdStoreOptions, opt.cache, opt.stores...)
@@ -75,11 +106,11 @@ func runMtree(ctx context.Context, opt mtreeOptions, args []string) error {
 	}
 	defer s.Close()
 
-	// Apparently the input must be an index, read it whole
+	// The input must be an index, read it whole
 	index, err := readCaibxFile(input, opt.cmdStoreOptions)
 	if err != nil {
 		return err
 	}
 
-	return desync.UnTarIndex(ctx, fs, index, s, opt.n, nil)
+	return desync.UnTarIndex(ctx, mtreeFS, index, s, opt.n, nil)
 }
