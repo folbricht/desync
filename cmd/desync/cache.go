8eb0446ef1cc85d2ecbefda3d2f430c5373a9cbb
Frank Olbricht <frank.olbricht@gmail.com>
2021-08-29 19:09:10 -0600

    Add --ignore and --ignore-chunks options to cache command (#197)
    * Add --ignore and --ignore-chunks options to cache command
    
    * Update doc

--- cmd/desync/cache.go
+++ cmd/desync/cache.go
@@ -10,8 +10,10 @@ import (
 
 type cacheOptions struct {
 	cmdStoreOptions
-	stores []string
-	cache  string
+	stores        []string
+	cache         string
+	ignoreIndexes []string
+	ignoreChunks  []string
 }
 
 func newCacheCommand(ctx context.Context) *cobra.Command {
@@ -23,7 +25,12 @@ func newCacheCommand(ctx context.Context) *cobra.Command {
 		Long: `Read chunk IDs from caibx or caidx files from one or more stores without
 writing to disk. Can be used (with -c) to populate a store with desired chunks
 either to be used as cache, or to populate a store with chunks referenced in an
-index file. Use '-' to read (a single) index from STDIN.`,
+index file. Use '-' to read (a single) index from STDIN.
+
+To exclude chunks that are known to exist in the target store already, use
+--ignore <index> which will skip any chunks from the given index. The same can
+be achieved by providing the chunks in their ASCII representation in a text
+file with --ignore-chunks <file>.`,
 		Example: `  desync cache -s http://192.168.1.1/ -c /path/to/local file.caibx`,
 		Args:    cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -34,6 +41,8 @@ index file. Use '-' to read (a single) index from STDIN.`,
 	flags := cmd.Flags()
 	flags.StringSliceVarP(&opt.stores, "store", "s", nil, "source store(s)")
 	flags.StringVarP(&opt.cache, "cache", "c", "", "target store")
+	flags.StringSliceVarP(&opt.ignoreIndexes, "ignore", "", nil, "index(s) to ignore chunks from")
+	flags.StringSliceVarP(&opt.ignoreChunks, "ignore-chunks", "", nil, "ignore chunks from text file")
 	addStoreOptions(&opt.cmdStoreOptions, flags)
 	return cmd
 }
@@ -58,6 +67,30 @@ func runCache(ctx context.Context, opt cacheOptions, args []string) error {
 		}
 		for _, c := range c.Chunks {
 			idm[c.ID] = struct{}{}
+		}
+	}
+	// If requested, skip/ignore all chunks that are referenced in other indexes or text files
+	if len(opt.ignoreIndexes) > 0 || len(opt.ignoreChunks) > 0 {
+		// Remove chunks referenced in indexes
+		for _, f := range opt.ignoreIndexes {
+			i, err := readCaibxFile(f, opt.cmdStoreOptions)
+			if err != nil {
+				return err
+			}
+			for _, c := range i.Chunks {
+				delete(idm, c.ID)
+			}
+		}
+
+		// Remove chunks referenced in ASCII text files
+		for _, f := range opt.ignoreChunks {
+			ids, err := readChunkIDFile(f)
+			if err != nil {
+				return err
+			}
+			for _, id := range ids {
+				delete(idm, id)
+			}
 		}
 	}
 
