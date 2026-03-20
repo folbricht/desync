9825a6abd36928add1e4ffa37351479fe1f261d0
folbrich <frank.olbricht@gmail.com>
2026-03-20 18:04:06 +0100

    cleanup

--- cmd/desync/extract.go
+++ cmd/desync/extract.go
@@ -45,9 +45,10 @@ the index from STDIN. If a seed is invalid, by default the extract operation wil
 aborted. With --skip-invalid-seeds, the invalid seeds will be discarded and the
 extraction will continue without them. Otherwise with --regenerate-invalid-seeds,
 any invalid seed indexes will be regenerated, in memory, by using the
-available data, and neither data nor indexes will be changed on disk. Also, if the seed changes
-while processing, its invalid chunks will be taken from the self seed, or the store, instead
-of aborting.`,
+available data, and neither data nor indexes will be changed on disk.
+Seeds are validated once before extraction begins. If a seed file is modified while
+the extraction is running, it could result in a corrupted target file. In such cases,
+use the verify-index command to check the integrity of the target afterwards.`,
 		Example: `  desync extract -s http://192.168.1.1/ -c /path/to/local file.caibx largefile.bin
   desync extract -s /mnt/store -s /tmp/other/store file.tar.caibx file.tar
   desync extract -s /mnt/store --seed /mnt/v1.caibx v2.caibx v2.vmdk
